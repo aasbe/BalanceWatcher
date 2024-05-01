645faf22e4176e515bb509e07c6577bc9c0e411e
aasbe <168559392+aasbe@users.noreply.github.com>
2024-05-01 23:41:19 +0200

    Update FXMLDocumentController.java

--- src/javafxmlapplication/FXMLDocumentController.java
+++ src/javafxmlapplication/FXMLDocumentController.java
@@ -30,7 +30,7 @@ public class FXMLDocumentController implements Initializable {
     //                      when the button has the focus and enter is pressed
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        labelMessage.setText("Hello, this is your first JavaFX project - IPC");
+        labelMessage.setText("Cambio 2 a ver!");
     }
     
     //=========================================================
