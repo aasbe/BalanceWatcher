3e549efdd6e6978d3bd68439c75f8b5a420ff64b
aasbe <168559392+aasbe@users.noreply.github.com>
2024-05-04 23:10:48 +0200

    Update JavaFXMLApplication.java

--- src/javafxmlapplication/JavaFXMLApplication.java
+++ src/javafxmlapplication/JavaFXMLApplication.java
@@ -28,7 +28,7 @@ public class JavaFXMLApplication extends Application {
         //     - configuracion del stage
         //     - se muestra el stage de manera no modal mediante el metodo show()
         stage.setScene(scene);
-        stage.setTitle("start PROJECT - IPC:");
+        stage.setTitle("BalanceWatcher");
         stage.show();
     }
 
