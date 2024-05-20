271ad6960b85ac0abe38ca3111c4276d25e4e288
aasbe <168559392+aasbe@users.noreply.github.com>
2024-05-20 11:46:24 +0200

    Login

--- src/javafxmlapplication/AutenticacionController.java
+++ src/javafxmlapplication/AutenticacionController.java
@@ -14,10 +14,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.Acount;
+import model.AcountDAOException;
 
 /**
  * FXML Controller class
@@ -30,6 +35,12 @@ public class AutenticacionController implements Initializable {
     private Button volverB;
     @FXML
     private Button accederB;
+    @FXML
+    private TextField usuario;
+    @FXML
+    private PasswordField password;
+    
+    private boolean accedido = false;
     
     private Stage stage;
     private Scene scene;
@@ -45,13 +56,27 @@ public class AutenticacionController implements Initializable {
 
     
     @FXML
-    public void acceder(javafx.event.ActionEvent event) throws IOException {
+    public void acceder(javafx.event.ActionEvent event) throws IOException, AcountDAOException {
+        /*LOGIN */
+        accedido = Acount.getInstance().logInUserByCredentials(usuario.getText(),password.getText());
+        
+        if (accedido)   {    
         root =  FXMLLoader.load(getClass().getResource("PRINCIPAL.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); 
         Scene scene = new Scene(root,800,400);
         stage.setScene(scene);
         stage.setTitle("Principal");
         stage.show();
+        }
+        
+        else{
+          Alert errorAu = new Alert(AlertType.ERROR);
+                errorAu.setTitle("Error");
+                errorAu.setHeaderText("Nombre de usuario/contraseña no registrados");
+                errorAu.showAndWait();
+                usuario.clear();
+                password.clear();   
+        }
     }
     @FXML
     public void volver(javafx.event.ActionEvent event) throws IOException {
