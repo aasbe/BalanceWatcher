e1d257e3390b8e5567df63e04f0b1dbe14978a6f
aasbe <168559392+aasbe@users.noreply.github.com>
2024-05-23 17:05:55 +0200

    Pantalla verCategorias

--- src/javafxmlapplication/VerCategoriasController.java
+++ src/javafxmlapplication/VerCategoriasController.java
@@ -2,7 +2,11 @@ package javafxmlapplication;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,10 +14,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import model.Acount;
+import model.AcountDAOException;
+import model.Category;
 
-public class PRINCIPALController implements Initializable {
+public class VerCategoriasController implements Initializable {
 
     private Stage stage;
     private Scene scene;
@@ -26,18 +35,37 @@ public class PRINCIPALController implements Initializable {
     @FXML
     private Button addGas;
     @FXML
-    private Button verCat;
-    @FXML
     private Button addCat;
+    @FXML
+    private Button verCat;
     @FXML
     private Button perfil; // Botón para acceder al perfil
     @FXML
     private Button volver;
+    @FXML
+    private ListView listaCategorias;
+    
+    private ObservableList<Category> todasCategorias;
+    private List <Category> todasCat;
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        /*
+            try {
+        todasCat= (Acount.getInstance().getUserCategories());
+        todasCategorias.setAll(todasCat);
+        }
+        catch (AcountDAOException error) {
+             Alert errorUs = new Alert(Alert.AlertType.ERROR);
+                errorUs.setTitle("Error");
+                errorUs.setHeaderText("No hay categorías de gastos registradas");
+                errorUs.showAndWait();
+        } catch (IOException ex) {
+            Logger.getLogger(GastoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            listaCategorias.setItems(todasCategorias);
+*/
     }
 
     public void addGasto(ActionEvent event) throws IOException {
@@ -50,22 +78,22 @@ public class PRINCIPALController implements Initializable {
     }
 
     @FXML
+    public void irPrincipal(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("PRINCIPAL.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); 
+        Scene scene = new Scene(root,800,400);
+        stage.setScene(scene);
+        stage.setTitle("Principal");
+        stage.show();
+    }
+    
+    @FXML
     public void irPerfil(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Perfil.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); 
         Scene scene = new Scene(root,800,400);
         stage.setScene(scene);
         stage.setTitle("PRINCIPAL - Perfil");
-        stage.show();
-    }
-    
-    @FXML
-    public void irVerCategorias(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("VerCategorias.fxml"));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); 
-        Scene scene = new Scene(root,800,400);
-        stage.setScene(scene);
-        stage.setTitle("PRINCIPAL - Categorías");
         stage.show();
     }
     
