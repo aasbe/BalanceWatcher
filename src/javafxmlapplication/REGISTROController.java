b81cb17eaf57553dc530f9f658bc314bdc82d0a1
caralovell <168685711+caralovell@users.noreply.github.com>
2024-05-26 22:37:24 +0200

    Update REGISTROController.java

--- src/javafxmlapplication/REGISTROController.java
+++ src/javafxmlapplication/REGISTROController.java
@@ -1,5 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package javafxmlapplication;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
@@ -23,6 +28,11 @@ import javafx.stage.Stage;
 import model.Acount;
 import model.AcountDAOException;
 
+/**
+ * FXML Controller class
+ *
+ * @author caral
+ */
 public class REGISTROController implements Initializable {
 
     @FXML
@@ -36,21 +46,19 @@ public class REGISTROController implements Initializable {
     @FXML
     private PasswordField contraseña;
     @FXML
-    private TextField mostrarcontraseña;
-    @FXML
     private Button siguiente;
     @FXML
     private Button atras;
     @FXML
     private Button selecciona;
     @FXML
-    private ImageView avatar;
+    private ImageView avatar; /*Para que se vea que funciona seleccionar la 
+    imagen, lo podemos quitar
+    */
+    
     @FXML
-    private ImageView ojoImageView;
-
-    @FXML
-    Image image = new Image("default.png");
-
+    Image image = new Image ("default.png");
+    
     private boolean isDisabled;
     private boolean newuser = false;
     String Nombre;
@@ -58,144 +66,145 @@ public class REGISTROController implements Initializable {
     String Correo;
     String Usuario;
     String Contraseña;
-
+    
     private Stage stage;
     private Scene scene;
     private Parent root;
 
+    
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        siguiente.setDisable(true);
-        mostrarcontraseña.setVisible(false);
-        mostrarcontraseña.setManaged(false);
-    }
-
+    siguiente.setDisable(true);   
+    }    
+    
     @FXML
     public void newuser(javafx.event.ActionEvent event) throws IOException {
+        
         Nombre = nombre.getText();
         Apellido = apellido.getText();
         Correo = correo.getText();
-        Usuario = usuario.getText();
+        Usuario = usuario.getText(); /*ÚNICO: Avisar al usuario?*/
         Contraseña = contraseña.getText();
-        isDisabled = (Nombre.isEmpty() || Correo.isEmpty() || Usuario.isEmpty() || Contraseña.isEmpty() || Apellido.isEmpty());
+        isDisabled = (Nombre.isEmpty()||Correo.isEmpty()||Usuario.isEmpty()||Contraseña.isEmpty()||Apellido.isEmpty());
         siguiente.setDisable(isDisabled);
-
-        try {
-            if (isDisabled || Usuario.contains(" ") || Contraseña.length() < 7) {
-                if (isDisabled) {
-                    Alert error = new Alert(AlertType.ERROR);
-                    error.setTitle("Error");
-                    error.setHeaderText("No ha rellenado todos los campos obligatorios * ");
-                    error.showAndWait();
-                }
-
-                if (Usuario.contains(" ")) {
-                    Alert error = new Alert(AlertType.ERROR);
-                    error.setTitle("Error");
-                    error.setHeaderText("El usuario no ha de tener espacios");
-                    error.showAndWait();
-                    usuario.clear();
-                    siguiente.setDisable(true);
-                }
-
-                if (Contraseña.length() < 7) {
-                    Alert error = new Alert(AlertType.ERROR);
-                    error.setTitle("Error");
-                    error.setHeaderText("La contraseña ha de tener mínimo 7 carácteres");
-                    error.showAndWait();
-                    contraseña.clear();
-                    siguiente.setDisable(true);
-                }
-            } else {
-                LocalDate fecha = LocalDate.now();
-
-                newuser = Acount.getInstance().registerUser(Nombre,
-                        Apellido, Correo, Usuario, Contraseña, image, fecha);
-                System.out.println(Usuario + " registrado!");
-
-                if (newuser) {
-                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                    alert.setTitle("Usuario nuevo registrado");
-                    alert.setHeaderText("Se ha registrado correctamente el usuario " + Usuario + ". Pulse 'Aceptar' para ir a la ventana de Autenticación.");
-                    if (alert.showAndWait().get() == ButtonType.OK) {
-                        irAutenticacion(event);
-                    }
-                }
+        
+        try{
+        /*CONDICIONES, 3 mensajes de error*/
+        if (isDisabled ||Usuario.contains(" ")||Contraseña.length()<7){
+            
+            if (isDisabled) {
+            Alert error = new Alert (AlertType.ERROR);
+            error.setTitle("Error");
+            error.setHeaderText("No ha rellenado todos los campos obligatorios * ");
+            error.showAndWait();
             }
-        } catch (AcountDAOException error) {
-            Alert errorUs = new Alert(AlertType.ERROR);
-            errorUs.setTitle("Error");
-            errorUs.setHeaderText("Nombre de usuario ya registrado");
-            errorUs.showAndWait();
-            usuario.clear();
+        
+            if (Usuario.contains(" ")){
+                Alert error = new Alert(AlertType.ERROR);
+                error.setTitle("Error");
+                error.setHeaderText("El usuario no ha de tener espacios");
+                error.showAndWait();
+                usuario.clear();
+                siguiente.setDisable(true);
+            }
+            
+            if(Contraseña.length()<7){
+                Alert error = new Alert(AlertType.ERROR);
+                error.setTitle("Error");
+                error.setHeaderText("La contraseña ha de tener mínimo 7 carácteres");
+                error.showAndWait();
+                contraseña.clear();
+                siguiente.setDisable(true);
+            }
         }
+         
+        /*REGISTRO*/
+        else {
+            LocalDate fecha = LocalDate.now();
+            
+        newuser = Acount.getInstance().registerUser(Nombre,
+            Apellido,Correo,Usuario,Contraseña, image,fecha);
+    System.out.println(Usuario+" registrado!"); 
+    
+    /* -> METER VENTANA y autenticar */
+    
+    if (newuser == true){ 
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Usuario nuevo registrado");
+        alert.setHeaderText("Se ha registrado correctamente el usuario "+ Usuario +". Pulse 'Aceptar' para ir a la ventana de Autenticación.");
+       /* alert.setContentText("Se ha registrado el nuevo usuario "+ Usuario +". Pulse aceptar para ir a la ventana de Autenticación");*/
+        if (alert.showAndWait().get() == ButtonType.OK) {
+        irAutenticacion(event);}
+    }
+        }     
+        }
+      catch  (AcountDAOException error) {
+          Alert errorUs = new Alert(AlertType.ERROR);
+                errorUs.setTitle("Error");
+                errorUs.setHeaderText("Nombre de usuario ya registrado");
+                errorUs.showAndWait();
+                usuario.clear();
+         /* System.out.println("Usuario ya registrado");*/
+      }   
     }
 
+    
     @FXML
-    public void SeleccionarAvatar(javafx.event.ActionEvent event) throws IOException {
+    public void SeleccionarAvatar (javafx.event.ActionEvent event) throws IOException {
+        
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Avatares.fxml"));
-        root = loader.load();
-        stage = new Stage();
+        root =  loader.load();
+        stage = new Stage();/*(Stage) ((Node)event.getSource()).getScene().getWindow(); En pop-up*/
+        /*PARA PASAR OBJETOS ENTRE CONTROLLERS*/
         AvataresController avatarescontroller = loader.getController();
         avatarescontroller.introducirImagen(image);
-
+        
         stage.setResizable(false);
-        Scene scene = new Scene(root, 800, 400);
+        Scene scene = new Scene(root,800,400);
         stage.setScene(scene);
         stage.setTitle("Foto de perfil");
-        stage.showAndWait();
+        stage.showAndWait(); /*CLAVE EL SHOWANDWAIT*/
         if (avatarescontroller.isISelected()) {
-            image = avatarescontroller.getImage();
-            avatar.setImage(image);
-        }
-    }
-
+            image= avatarescontroller.getImage();
+            avatar.setImage(image);}
+    } 
+    
     @FXML
-    public void keyReleasedProperty() {
+    public void keyReleasedProperty() {  /*Si lo ponemos en en los textfields, lo checkea cada vez que sueltas una tecla, así no hace falta
+                                        el listener */
         Nombre = nombre.getText();
         Apellido = apellido.getText();
         Correo = correo.getText();
-        Usuario = usuario.getText();
+        Usuario = usuario.getText(); /*ÚNICO: Avisar al usuario?*/
         Contraseña = contraseña.getText();
-        isDisabled = (Nombre.isEmpty() || Correo.isEmpty() || Usuario.isEmpty() || Contraseña.isEmpty() || Apellido.isEmpty());
+        isDisabled = (Nombre.isEmpty()||Correo.isEmpty()||Usuario.isEmpty()||Contraseña.isEmpty()||Apellido.isEmpty());
         siguiente.setDisable(isDisabled);
     }
-
+    
+     
     @FXML
     public void volver(javafx.event.ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Inicio.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, 800, 400);
+        root =  FXMLLoader.load(getClass().getResource("Inicio.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); 
+        Scene scene = new Scene(root,800,400);
         stage.setScene(scene);
         stage.setTitle("BalanceWatcher");
         stage.show();
-    }
-
+    } 
+    
     @FXML
     public void irAutenticacion(javafx.event.ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Autenticacion.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, 800, 400);
+        root =  FXMLLoader.load(getClass().getResource("Autenticacion.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); 
+        Scene scene = new Scene(root,800,400);
         stage.setScene(scene);
         stage.setTitle("Autenticación");
         stage.show();
-    }
-
-    @FXML
-    public void mostrarContraseña(MouseEvent event) {
-        mostrarcontraseña.setText(contraseña.getText());
-        mostrarcontraseña.setVisible(true);
-        mostrarcontraseña.setManaged(true);
-        contraseña.setVisible(false);
-        contraseña.setManaged(false);
-    }
-
-    @FXML
-    public void ocultarContraseña(MouseEvent event) {
-        contraseña.setText(mostrarcontraseña.getText());
-        contraseña.setVisible(true);
-        contraseña.setManaged(true);
-        mostrarcontraseña.setVisible(false);
-        mostrarcontraseña.setManaged(false);
-    }
+    } 
+    
 }
